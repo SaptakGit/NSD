2ab1cdd41dc5d0c3df57027fb1a564745146b99e
SaptakGit <81002606+SaptakGit@users.noreply.github.com>
2021-08-21 11:05:57 +0530

    Update database.php

--- php/database.php
+++ php/database.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header("Content-Type: application/json; charset=UTF-8");
 $db_host = 'localhost';
-$db_username = 'root';
+$db_username = 'root'; 
 $db_password = '';
 $db_name = 'newostreet_db';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
